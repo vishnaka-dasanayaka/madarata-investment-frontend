bbd6ed581d7c5b0e673ceef67ce78a04d98f70a1
Vishnaka Dasanayaka <vishnakadasanayaka@gmail.com>
2026-01-29 23:47:40 +0530

    Initial commit

---
+++ src/app/layout/header/header.component.ts
@@ -0,0 +1,62 @@
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import { AuthenticationService } from "../../core/_services/authentication.service";
+
+@Component({
+  selector: "app-header",
+  standalone: false,
+  templateUrl: "./header.component.html",
+  styleUrl: "./header.component.css",
+})
+export class HeaderComponent {
+  sysuser: any;
+  private sub: any;
+
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) {}
+
+  ngOnInit(): void {
+    console.log("jhjh");
+
+    console.log("AuthService", this.authenticationService);
+
+    this.sub = this.authenticationService
+      .validateUser()
+      .subscribe((sysuser) => {
+        this.sysuser = sysuser;
+
+        // if (
+        //   this.sysuser.app_settings.admin_settings.header_name &&
+        //   this.sysuser.app_settings.admin_settings.header_name != null
+        // ) {
+        //   this.header_name = this.sysuser.app_settings.admin_settings.header_name;
+        // }
+        // if (
+        //   this.sysuser.app_settings.admin_settings.header_name &&
+        //   this.sysuser.app_settings.admin_settings.header_name != null
+        // ) {
+        //   this.titleService.setTitle(
+        //     this.sysuser.app_settings.admin_settings.index_page_name
+        //   );
+        // } else {
+        //   this.titleService.setTitle("SISKA");
+        // }
+        // if (
+        //   this.settings.getAppSetting("version") <
+        //   sysuser.codevus_settings[0].s_key
+        // ) {
+        //   //check for updates
+        //   this.blockUI.start(
+        //     "Upgrade to the newest, improved version of our software now!  Installing version " +
+        //       sysuser.codevus_settings[0].s_key +
+        //       " in a moment.."
+        //   );
+        //   setTimeout(function () {
+        //     location.reload();
+        //   }, 5000);
+        // }
+      });
+  }
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  // baseUrl: "https://madaratainvestment.lk/",
+  baseUrl: "http://localhost:1337/",
+};
