79017293d052a2491fcd74ffb3662e330e24d3dc
Vishnaka Dasanayaka <vishnakadasanayaka@gmail.com>
2026-03-23 06:39:02 +0530

    LOAN FILES 2

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -26,6 +26,14 @@ export const serverRoutes: ServerRoute[] = [
       return ids.map((id) => ({ id }));
     },
   },
+  {
+    path: "loans/loan-details/:id",
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+      const ids = routesIDs;
+      return ids.map((id) => ({ id }));
+    },
+  },
 
   // sethneth
 
